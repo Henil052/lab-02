7206cb561aed472e68688aba8e77b7416cce399b
Henil Patel <henilpatel0502@gmail.com>
2026-01-16 13:21:47 -0700

    Completed Lab2

--- ListCity/app/src/main/java/com/example/listcity/MainActivity.java
+++ ListCity/app/src/main/java/com/example/listcity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.listcity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import androidx.appcompat.app.AlertDialog;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
-
+    int selectedIndex = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +36,56 @@ public class MainActivity extends AppCompatActivity {
 
         cityAdapter = new ArrayAdapter<>(
                 this,
-                android.R.layout.simple_list_item_1,
+                android.R.layout.simple_list_item_activated_1,
                 dataList
         );
 
         cityList.setAdapter(cityAdapter);
+
+        cityList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+        // Making the AddCity button functioning
+        Button addCitybtn=findViewById(R.id.leftButton);
+
+        addCitybtn.setOnClickListener(v -> {
+
+            EditText input = new EditText(this);
+            input.setHint("Enter city name");
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Add City")
+                    .setView(input)
+                    .setPositiveButton("Add", (dialog, which) -> {
+                        String city = input.getText().toString().trim();
+                        if (!city.isEmpty()) {
+                            dataList.add(city);
+                            cityAdapter.notifyDataSetChanged();
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+        });
+
+        // Making the Delete Button Functioning
+
+        Button deleteCitybt = findViewById(R.id.rightButton);
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedIndex = position;
+        });
+
+
+        deleteCitybt.setOnClickListener(v -> {
+            if (selectedIndex != -1) {
+                dataList.remove(selectedIndex);
+                cityAdapter.notifyDataSetChanged();
+                selectedIndex = -1; // reset
+                cityList.clearChoices();
+                cityAdapter.notifyDataSetChanged();
+
+            }
+        });
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
